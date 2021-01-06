210408f6c44fbd1ad44efa453591a8cffcc5797c
Denis Egorov <denis42914@gmail.com>
2021-01-06 20:35:42 +0300

    point of works well infra with ssh-key connect and network connectivity between instances

--- infra.tf
+++ infra.tf
@@ -1,3 +1,31 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_subnet" "subnet-main" {
+  cidr_block = "10.0.0.0/16"
+  vpc_id = aws_vpc.main.id
+  availability_zone = "eu-central-1a"
+}
+
+resource "aws_internet_gateway" "gw-main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "route-table-main" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw-main.id
+  }
+}
+
+resource "aws_route_table_association" "subnet-association" {
+  subnet_id      = aws_subnet.subnet-main.id
+  route_table_id = aws_route_table.route-table-main.id
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDA8krVo3NlA7el7qHdz4DA/bR+pJ2cDVhK94k8ExMcPFII3iwB3YSBuzN7zlL9YhD1eW/4mYhigu5qRkumo43V5sx1w4DFwAWZF5CDWWc2wruNu22xJjKfpyBAUjH+Bz6YyGy/x+VGrohIfY5LqPUqeBFopjYcoSx/OOmpLqIp+VJrYWaLLdFfAwaurswnqYWIdVjPRnWgKw2W76hAAWxJEIJf0luNptJeS27vQtYdr3zqdjJamDz2hRSqwa0YJ2QYlPq5N+XIns+PfLMAhR0BtWEkHYoB4lkPiZKQsxlO77wKbvV36VyHCwsEHRViPqsiaqkd/DsCTzJVjlrvn7sE9F4IaoH53oPBvGcMUO+vUZZHFPUfJWWl6Rqm+s7tm92PGKntJiHcAo/FZLOmUGzfUJd8S8ey6QtRSvIB8Ez8ZSkA2rfhtr+Cd4maOJm5hG8QM77PhtIBoPhuE39drZeSeYLtX7lqbzg+Tjzr730q0UpGOXQNh5tzvgYLM98Btgk= denis42914@centos8"
+}
--- sg.tf
+++ sg.tf
@@ -1,9 +1,9 @@
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
-
+  vpc_id      = aws_vpc.main.id
   ingress {
-    description = "SSH from Ansible host"
+    description = "SSH inside VPC"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
