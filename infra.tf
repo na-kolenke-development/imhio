e308f5126c3aca42c7d92d720d0d660fd86db6b3
na-kolenke-development <77058806+na-kolenke-development@users.noreply.github.com>
2021-09-12 05:29:25 +0300

    fix naming according to tf convention

--- infra.tf
+++ infra.tf
@@ -1,18 +1,18 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "this" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "subnet-main" {
+resource "aws_subnet" "this" {
   cidr_block = "10.0.0.0/16"
   vpc_id = aws_vpc.main.id
   availability_zone = "eu-central-1a"
 }
 
-resource "aws_internet_gateway" "gw-main" {
+resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_route_table" "route-table-main" {
+resource "aws_route_table" "this" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
@@ -20,7 +20,7 @@ resource "aws_route_table" "route-table-main" {
   }
 }
 
-resource "aws_route_table_association" "subnet-association" {
+resource "aws_route_table_association" "this" {
   subnet_id      = aws_subnet.subnet-main.id
   route_table_id = aws_route_table.route-table-main.id
 }
